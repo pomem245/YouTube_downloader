5b2c1342ad0be094580d20a58dc041f3a1dc7d37
pomem245 <145405702+pomem245@users.noreply.github.com>
2024-07-19 12:47:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,10 @@ start = (dt.date.today() - dt.timedelta(days=1104)).strftime('%Y-%m-%d')
 
 df = yf.download('AAPL', start=start , end=date_now)
 df = df.to_csv(df)
-# Defaults to "application/octet-stream"
-st.download_button("Download binary file", df)
+
+st.download_button(
+    label="Download data as CSV",
+    data=df,
+    file_name="large_df.csv",
+    mime="text/csv",
+)
