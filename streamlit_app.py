c9cfebc86458fb156f58350fc2c564e5401ef2b1
pomem245 <145405702+pomem245@users.noreply.github.com>
2024-07-19 12:39:48 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,13 @@
-import streamlit as st
-import yt_dlp
+import datetime as dt
+import time as tm
+import yfinance as yf
 
-def download_audio(link):
-    ydl_opts = {
-        'ignoreerrors': True,
-        'extract_audio': True,
-        'format': 'bestaudio',
-        'outtmpl': '%(title)s.mp3'
-    }
-    
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info_dict = ydl.extract_info(link, download=True)
-        
-        if info_dict:
-            video_title = info_dict.get('title', 'Unknown Title')
-            st.write(f"Successfully downloaded: {video_title}")
-            with open(f"{video_title}.mp3", "rb") as file:
-                audio_data = file.read()
-                st.download_button("Download Audio", audio_data, file_name=f"{video_title}.mp3", mime="audio/mpeg")
-        else:
-            st.write("There was an error downloading the audio.")
+# Current date
+date_now = tm.strftime('%Y-%m-%d')
+start = (dt.date.today() - dt.timedelta(days=1104)).strftime('%Y-%m-%d')
 
-def main():
-    st.title("YouTube Audio Downloader")
-    url = st.text_input("Enter the YouTube link here:")
-    if st.button("Download Audio"):
-        download_audio(url)
 
-if __name__ == "__main__":
-    main()
+df = yf.download('AAPL', start=start , end=date_now)
+
+# Defaults to "application/octet-stream"
+st.download_button("Download binary file", df)
