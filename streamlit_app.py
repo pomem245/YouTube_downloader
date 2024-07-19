ef8659b419ec52007dc623ea1a62456c5981eb9a
pomem245 <145405702+pomem245@users.noreply.github.com>
2024-07-19 12:46:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import datetime as dt
 import time as tm
 import yfinance as yf
 import streamlit as st
+import pandas as pd
 
 # Current date
 date_now = tm.strftime('%Y-%m-%d')
@@ -9,6 +10,6 @@ start = (dt.date.today() - dt.timedelta(days=1104)).strftime('%Y-%m-%d')
 
 
 df = yf.download('AAPL', start=start , end=date_now)
-
+df = df.to_csv(df)
 # Defaults to "application/octet-stream"
 st.download_button("Download binary file", df)
